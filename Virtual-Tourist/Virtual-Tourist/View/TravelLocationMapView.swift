cb0c39a9152a1accfb21beeeffc53c0bb63f3106
MRAO0212 <mayureshvrao@gmail.com>
2020-08-13 02:23:51 -0700

    add MkMapView

---
+++ Virtual-Tourist/Virtual-Tourist/View/TravelLocationMapView.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Mayuresh Rao on 8/12/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
